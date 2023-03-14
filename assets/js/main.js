8c9ece9e4840820c91949fbb6d747965fc1c59ea
AnaFern2023 <AnaFern2023@gmail.com>
2023-03-14 10:05:19 +0100

    last_step

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,49 +14,53 @@ const submitButton = () => {
         baseKcal.innerHTML = grundumsatzKcal.toFixed(0);
         let grundumsatzKj = grundumsatzKcal * 4.184;
         baseKj.innerHTML = grundumsatzKj.toFixed(0);
-        if (inputActivity.value == "0.95") {
-            totalKcal.innerHTML = (grundumsatzKcal * inputActivity.value).toFixed(0);
-            totalKj.innerHTML = (grundumsatzKj * inputActivity.value).toFixed(0);
-        } else if (inputActivity.value == "1.2") {
-            totalKcal.innerHTML = (grundumsatzKcal * inputActivity.value).toFixed(0);
-            totalKj.innerHTML = (grundumsatzKj * inputActivity.value).toFixed(0);
-        } else if (inputActivity.value == "1.5") {
-            totalKcal.innerHTML = (grundumsatzKcal * inputActivity.value).toFixed(0);
-            totalKj.innerHTML = (grundumsatzKj * inputActivity.value).toFixed(0);
-        } else if (inputActivity.value == "1.7") {
-            totalKcal.innerHTML = (grundumsatzKcal * inputActivity.value).toFixed(0);
-            totalKj.innerHTML = (grundumsatzKj * inputActivity.value).toFixed(0);
-        } else if (inputActivity.value == "1.9") {
-            totalKcal.innerHTML = (grundumsatzKcal * inputActivity.value).toFixed(0);
-            totalKj.innerHTML = (grundumsatzKj * inputActivity.value).toFixed(0);
-        } else if (inputActivity.value == "2.2") {
-            totalKcal.innerHTML = (grundumsatzKcal * inputActivity.value).toFixed(0);
-            totalKj.innerHTML = (grundumsatzKj * inputActivity.value).toFixed(0);
-        }
+        totalKcal.innerHTML = (grundumsatzKcal * inputActivity.value).toFixed(0);
+        totalKj.innerHTML = (grundumsatzKj * inputActivity.value).toFixed(0);
+        // if (inputActivity.value == "0.95") {
+        //     totalKcal.innerHTML = (grundumsatzKcal * inputActivity.value).toFixed(0);
+        //     totalKj.innerHTML = (grundumsatzKj * inputActivity.value).toFixed(0);
+        // } else if (inputActivity.value == "1.2") {
+        //     totalKcal.innerHTML = (grundumsatzKcal * inputActivity.value).toFixed(0);
+        //     totalKj.innerHTML = (grundumsatzKj * inputActivity.value).toFixed(0);
+        // } else if (inputActivity.value == "1.5") {
+        //     totalKcal.innerHTML = (grundumsatzKcal * inputActivity.value).toFixed(0);
+        //     totalKj.innerHTML = (grundumsatzKj * inputActivity.value).toFixed(0);
+        // } else if (inputActivity.value == "1.7") {
+        //     totalKcal.innerHTML = (grundumsatzKcal * inputActivity.value).toFixed(0);
+        //     totalKj.innerHTML = (grundumsatzKj * inputActivity.value).toFixed(0);
+        // } else if (inputActivity.value == "1.9") {
+        //     totalKcal.innerHTML = (grundumsatzKcal * inputActivity.value).toFixed(0);
+        //     totalKj.innerHTML = (grundumsatzKj * inputActivity.value).toFixed(0);
+        // } else if (inputActivity.value == "2.2") {
+        //     totalKcal.innerHTML = (grundumsatzKcal * inputActivity.value).toFixed(0);
+        //     totalKj.innerHTML = (grundumsatzKj * inputActivity.value).toFixed(0);
+        // }
 
     } else {
         let grundumsatzKcal = 655.1 + (9.6 * inputWeight.value) + (1.8 * inputHeight.value) - (4.7 * inputWeight.value);
         baseKcal.innerHTML = grundumsatzKcal.toFixed(0);
         let grundumsatzKj = grundumsatzKcal * 4.184;
         baseKj.innerHTML = grundumsatzKj.toFixed(0);
-        if (inputActivity.value == "0.95") {
-            totalKcal.innerHTML = (grundumsatzKcal * inputActivity.value).toFixed(0);
-            totalKj.innerHTML = (grundumsatzKj * inputActivity.value).toFixed(0);
-        } else if (inputActivity.value == "1.2") {
-            totalKcal.innerHTML = (grundumsatzKcal * inputActivity.value).toFixed(0);
-            totalKj.innerHTML = (grundumsatzKj * inputActivity.value).toFixed(0);
-        } else if (inputActivity.value == "1.5") {
-            totalKcal.innerHTML = (grundumsatzKcal * inputActivity.value).toFixed(0);
-            totalKj.innerHTML = (grundumsatzKj * inputActivity.value).toFixed(0);
-        } else if (inputActivity.value == "1.7") {
-            totalKcal.innerHTML = (grundumsatzKcal * inputActivity.value).toFixed(0);
-            totalKj.innerHTML = (grundumsatzKj * inputActivity.value).toFixed(0);
-        } else if (inputActivity.value == "1.9") {
-            totalKcal.innerHTML = (grundumsatzKcal * inputActivity.value).toFixed(0);
-            totalKj.innerHTML = (grundumsatzKj * inputActivity.value).toFixed(0);
-        } else if (inputActivity.value == "2.2") {
-            totalKcal.innerHTML = (grundumsatzKcal * inputActivity.value).toFixed(0);
-            totalKj.innerHTML = (grundumsatzKj * inputActivity.value).toFixed(0);
-        }
+        totalKcal.innerHTML = (grundumsatzKcal * inputActivity.value).toFixed(0);
+        totalKj.innerHTML = (grundumsatzKj * inputActivity.value).toFixed(0);
+        // if (inputActivity.value == "0.95") {
+        //     totalKcal.innerHTML = (grundumsatzKcal * inputActivity.value).toFixed(0);
+        //     totalKj.innerHTML = (grundumsatzKj * inputActivity.value).toFixed(0);
+        // } else if (inputActivity.value == "1.2") {
+        //     totalKcal.innerHTML = (grundumsatzKcal * inputActivity.value).toFixed(0);
+        //     totalKj.innerHTML = (grundumsatzKj * inputActivity.value).toFixed(0);
+        // } else if (inputActivity.value == "1.5") {
+        //     totalKcal.innerHTML = (grundumsatzKcal * inputActivity.value).toFixed(0);
+        //     totalKj.innerHTML = (grundumsatzKj * inputActivity.value).toFixed(0);
+        // } else if (inputActivity.value == "1.7") {
+        //     totalKcal.innerHTML = (grundumsatzKcal * inputActivity.value).toFixed(0);
+        //     totalKj.innerHTML = (grundumsatzKj * inputActivity.value).toFixed(0);
+        // } else if (inputActivity.value == "1.9") {
+        //     totalKcal.innerHTML = (grundumsatzKcal * inputActivity.value).toFixed(0);
+        //     totalKj.innerHTML = (grundumsatzKj * inputActivity.value).toFixed(0);
+        // } else if (inputActivity.value == "2.2") {
+        //     totalKcal.innerHTML = (grundumsatzKcal * inputActivity.value).toFixed(0);
+        //     totalKj.innerHTML = (grundumsatzKj * inputActivity.value).toFixed(0);
+        // }
     }
 }
